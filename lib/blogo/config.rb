78ee57eb40492fd040e096a78a93cc3c078b2cbf
root <root@ns357509.ip-91-121-149.eu>
2016-04-13 17:48:05 +0200

    layout

--- lib/blogo/config.rb
+++ lib/blogo/config.rb
@@ -14,6 +14,8 @@ module Blogo
     attr_accessor :disqus_shortname
     attr_accessor :twitter_username
     attr_accessor :facebook_username
+    attr_accessor :site_url 
+    attr_accessor :site_icon
     attr_accessor :github_username
     attr_accessor :show_rss_icon
     attr_accessor :google_analytics_id
